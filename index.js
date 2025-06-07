306cc5ba24a749c47b9855376d2b09b484eb2ce4
Francisco García <francisco@goltratec.com>
2025-06-07 19:25:29 +0200

    .

--- index.js
+++ index.js
@@ -2,21 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
-const {join} = require("node:path");
-const {nextSongToProcess} = require('./processSong');
-
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use('/videos', express.static(join(__dirname, 'downloads')));
-
+app.use(express.urlencoded({ extended: true }));
 const router = require('./router');
-const {ProcessStatusSong} = require("./constants");
 
 // Use the router
 app.use('/', router);
 
-setTimeout(nextSongToProcess, 5000);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);

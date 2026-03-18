455930ac8ec4847077a5d457ef4b3927b8c34c12
Francisco García <francisco@goltratec.com>
2026-03-18 13:08:56 +0100

    Revert "fichero para almacenar constantes. Se crean como constantes los diferentes estados que puede tener una canción."
    This reverts commit 57f6185661b94d02ad5aff546b192c3b2bbd77a5.

--- constants.js
+++
@@ -1,8 +0,0 @@
-const ProcessStatusSong = {
-    'Unprocessed': 'unprocessed',
-    'Processing': 'processing',
-    'Processed': 'processed',
-    'Failed': 'failed',
-}
-
-module.exports = {ProcessStatusSong}
